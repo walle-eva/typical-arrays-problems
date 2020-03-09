59143c50514b2b94b3a16ea54d89267b5e73d525
walle-eva <walle-eva@users.noreply.github.com>
2020-03-09 23:57:23 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = Infinity;
+    for(let i = 0; i , array.length; i++){
+      if(array[i] < min){
+        min = array[i]
+      }
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = -Infinity;
+    for(let i = 0; i < array.length; i++){
+      if(array[i] > max){
+        max = array[i]
+      }
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let suma = 0;
+    for(let i = 0; i < array.length; i++){
+      summa += arr[i]
+    }
+    return summa/array.length;
 }
